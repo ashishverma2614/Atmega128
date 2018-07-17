84ad470b1d4d48be4c203235a4c3ce4867bf14bb
William Woo <woo0327@outlook.com>
2018-07-17 14:58:04 +0900

    Extra configuration of library files used frequently

---
+++ BasicLibrary/UART.h
@@ -0,0 +1,13 @@
+﻿#ifndef UART_H_
+#define UART_H_
+
+typedef unsigned char unit8_t;
+
+void UART_init(unsigned int);
+void Put_ch(char);
+unsigned char Get_ch(void);
+void Put_str(const unsigned char*);
+void Put_1byte_num(unit8_t n);
+void Put_float_num(float);
+
+#endif /* UART_H_ */
