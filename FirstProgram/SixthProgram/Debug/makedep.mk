2c8cfd9545de307a9a5f82348ec4fde4ec888da0
William Woo <woo0327@outlook.com>
2018-08-31 00:39:55 +0900

    Make define directive for clear understanding & add derial debug code

--- FirstProgram/SixthProgram/Debug/makedep.mk
+++ FirstProgram/SixthProgram/Debug/makedep.mk
@@ -4,3 +4,5 @@
 
 main.c
 
+USART.c
+
--- FirstProgram/SixthProgram/main.c
+++ FirstProgram/SixthProgram/main.c
@@ -1,31 +1,71 @@
 #define F_CPU 16000000L
 #include <avr/io.h>
 #include <util/delay.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "USART.h"
 
+#define UBRR_9600_1X 103
+#define TRUE 1
+#define SW_ON 1
+#define SW_OFF 0
+
+// file redirection
+FILE OUTPUT = FDEV_SETUP_STREAM(USART0_PutCh, NULL, _FDEV_SETUP_WRITE);
+//FILE INPUT = FDEV_SETUP_STREAM(NULL, USART0_GetCh, _FDEV_SETUP_READ);
+void debug_LED(char led);
+///////////////////////////////////////////////////////////////////////////////
 
 int main(void)
 {
-	char led = 0x01; // INIT of led(first on)
-	char swState = 0;
+    // initialization
+    stdout = &OUTPUT;
+    USART0_Init(UBRR_9600_1X);
+	static char ledOnLocation = 0x10;
+	static char swState = SW_OFF;
 	
-	DDRB = 0xFF; // led, output
+	DDRB = 0xF0; // led, output
 	DDRD = 0x00; //SW, input
     PORTD = 0xFF; // internal pull-up resistor
-    while (1) 
+    
+    // execution loop
+    while (TRUE) 
     {
-		PORTB = led; // set led to PORTB
-		if ((PIND & 0x01) != 0 && swState == 0)
+		if ((PIND & 0x01) != 0 && swState == SW_OFF)
 		{
-			swState = 1; // change state of led
-			if (led == 0x08) // if 8th led is on
-				led = 0x01;
+            PORTB = ledOnLocation;
+			swState = SW_ON;
+            printf("Switch is pressed!: ");
+            debug_LED(ledOnLocation);
+			if (ledOnLocation == 0x80)
+				ledOnLocation = 0x10;
 			else
-				led <<= 1;
-
+				ledOnLocation <<= 1;
 		}
-		if ((PIND & 0x01) == 0 && swState == 1)
-			swState = 0;
-
+		if ((PIND & 0x01) == 0 && swState == SW_OFF)
+			swState = SW_OFF;
     }
+    return EXIT_SUCCESS;
 }
 
+/* function definition */
+
+void debug_LED(char led)
+{
+    switch(led) // serial monitor debug
+    {
+        case 0x10:
+        printf("LED ON: X X X O\r\n");
+        break;
+        case 0x20:
+        printf("LED ON: X X O X\r\n");
+        break;
+        case 0x40:
+        printf("LED ON: X O X X\r\n");
+        break;
+        case 0x80:
+        printf("LED ON: O X X X\r\n");
+        break;
+    }
+}
